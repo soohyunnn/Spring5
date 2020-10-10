2e5af17219c0477fbe397d1a18254b68135254dc
soohyunnn <sksmsqodn20@gmail.com>
2020-10-10 21:38:45 +0900

    9장 공부

---
+++ chapter09/boot-jta/src/main/java/com/apress/prospring5/ch9/services/SingerServiceImpl.java
@@ -0,0 +1,37 @@
+package com.apress.prospring5.ch9.services;
+
+import com.apress.prospring5.ch9.entities.Singer;
+import com.apress.prospring5.ch9.ex.AsyncXAResourcesException;
+import com.apress.prospring5.ch9.repos.SingerRepository;
+import org.springframework.jms.core.JmsTemplate;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+
+
+@Service("singerService")
+@Transactional
+public class SingerServiceImpl implements SingerService {
+
+	private SingerRepository singerRepository;
+	private JmsTemplate jmsTemplate;
+
+	public SingerServiceImpl(SingerRepository singerRepository, JmsTemplate jmsTemplate) {
+		this.singerRepository = singerRepository;
+		this.jmsTemplate = jmsTemplate;
+	}
+
+	@Override
+	public Singer save(Singer singer) {
+		jmsTemplate.convertAndSend("singers", "방금 저장됨:" + singer);
+		if(singer == null) {
+			throw new AsyncXAResourcesException("잘못된 상황의 시뮬레이션");
+		}
+		singerRepository.save(singer);
+		return singer;
+	}
+
+	@Override public long count() {
+		return singerRepository.count();
+	}
+}
