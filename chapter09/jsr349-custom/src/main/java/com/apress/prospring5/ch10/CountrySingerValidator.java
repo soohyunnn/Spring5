7f84aae20110663b0f213b03e424ad7a029c3aaf
soohyunnn <sksmsqodn20@gmail.com>
2020-10-12 21:10:31 +0900

    10장 정리 완료(2020.10.12)

---
+++ chapter09/jsr349-custom/src/main/java/com/apress/prospring5/ch10/CountrySingerValidator.java
@@ -0,0 +1,26 @@
+package com.apress.prospring5.ch10;
+
+import javax.validation.ConstraintValidator;
+import javax.validation.ConstraintValidatorContext;
+
+import com.apress.prospring5.ch10.obj.Singer;
+
+public class CountrySingerValidator implements ConstraintValidator<CheckCountrySinger, Singer>{
+
+	@Override
+	public void initialize(CheckCountrySinger constrainAnnotation) {
+		
+	}
+	
+	@Override
+	public boolean isValid(Singer singer, ConstraintValidatorContext context) {
+		boolean result = true;
+		if(singer.getGenre() != null &&
+				(singer.isCountrySinger() &&
+				(singer.getLastName() == null || singer.getGender() == null))) {
+			result = false;
+		}
+		return result;
+	}
+
+}
