626f8f54e66b5f4fbbfac4e10799e26e46f0ab3b
soohyunnn <sksmsqodn20@gmail.com>
2020-11-07 11:27:23 +0900

    16장 스프링 부트로 스프링 웹 어플리케이션 생성하기

---
+++ singer-webapp-boot/src/test/java/com/apress/prospring5/SingerWebappBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.apress.prospring5;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SingerWebappBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
